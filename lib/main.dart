b4ab5ed379acab75fecd54e97a01ddd76436bf31
Umair Ahmed <57555747+UmairAhmad981@users.noreply.github.com>
2025-03-05 22:51:06 +0530

    Api Key Secured

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:plantfo/home.dart';
 List<CameraDescription> cameras = [];
-const apiKey = 'AIzaSyCu-GReY4mvDJXDgP8Ae3luG2V5OnjO1LM';
+// const apiKey = 'AIzaSyCu-GReY4mvDJXDgP8Ae3luG2V5OnjO1LM';
 Future<void> main() async{
+  await dotenv.load(fileName: ".env");
+  final apiKey=dotenv.env['API_KEY'];
+  print(apiKey);
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  Gemini.init(apiKey: apiKey);
+  Gemini.init(apiKey: apiKey!);
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
